40191907121ea348597f1b0c8834117b3cbac7e1
cr700me-creator <cr700.me@gmail.com>
2026-07-19 20:44:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -137,3 +137,39 @@ if (locationBtn) {
     });
 
 }
+// =========================
+// معرض الصور
+// =========================
+
+const galleryImages = document.querySelectorAll(".gallery-grid img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImage = document.getElementById("lightboxImage");
+const closeLightbox = document.getElementById("closeLightbox");
+
+galleryImages.forEach((image) => {
+
+    image.addEventListener("click", () => {
+
+        lightbox.style.display = "flex";
+
+        lightboxImage.src = image.src;
+
+    });
+
+});
+
+closeLightbox.addEventListener("click", () => {
+
+    lightbox.style.display = "none";
+
+});
+
+lightbox.addEventListener("click", (e) => {
+
+    if (e.target === lightbox) {
+
+        lightbox.style.display = "none";
+
+    }
+
+});
